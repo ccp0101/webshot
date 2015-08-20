857020d734fdd99738909549f01171ff7fe481a1
Changping Chen <ccp0101@gmail.com>
2015-08-21 02:44:23 +0900

    initial web

---
+++ main.py
@@ -0,0 +1,92 @@
+import tornado.web
+import tornado.log
+import tornado.gen
+import tornado.process
+import tornado.options
+import os
+import distutils.spawn
+import mimetypes
+import logging
+import time
+
+
+mimetypes.init()
+
+PHANTOMJS_BIN = distutils.spawn.find_executable("phantomjs")
+assert os.path.isfile(PHANTOMJS_BIN)
+
+API_DIR = os.path.dirname(__file__)
+SCRIPT_PATH = os.path.join(API_DIR, "rasterize.js")
+
+VALID_FORMATS = {
+    "png": mimetypes.types_map['.png'],
+    "jpg": mimetypes.types_map['.jpg'],
+    "pdf": mimetypes.types_map['.pdf']
+}
+
+
+class HelloHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.finish("Hello world")
+
+
+class WebshotHandler(tornado.web.RequestHandler):
+    def __init__(self, *args, **kwargs):
+        tornado.web.RequestHandler.__init__(self, *args, **kwargs)
+        self.url = None
+        self.width = None
+        self.format = None
+        self.zoom = None
+        self.start_ts = None
+
+    @tornado.gen.coroutine
+    def get(self):
+        self.url = self.get_argument("url")
+        self.width = self.get_argument("width", "1200px")
+        self.format = self.get_argument("format", "png")
+        self.zoom = int(self.get_argument("zoom", "1"))
+        self.start_ts = time.time()
+
+        assert self.url
+        assert self.format in VALID_FORMATS
+        assert self.width.endswith("px")
+
+        cmd = map(str, [PHANTOMJS_BIN, SCRIPT_PATH, self.url, "/dev/stdout", self.width, self.format, self.zoom])
+        logging.info("Executing: " + str(cmd))
+        p = tornado.process.Subprocess(cmd, stdout=tornado.process.Subprocess.STREAM)
+        p.stdout.read_until_close(streaming_callback=self.write_data)
+        return_code = yield p.wait_for_exit(raise_error=False)
+
+        if return_code == 0:
+            self.finish()
+        else:
+            self.send_error()
+
+    def write_data(self, data):
+        if data:
+            if not self._headers_written:
+                self.set_header("X-Requested-URL", str(self.url))
+                self.set_header("X-Requested-Width", self.width)
+                self.set_header("X-Requested-Format", self.format)
+                self.set_header("X-Requested-Zoom", str(self.zoom))
+                self.set_header("Content-Type", VALID_FORMATS[self.format])
+
+                end_ts = time.time()
+                secs = end_ts - self.start_ts
+                self.set_header("X-Perf-Seconds", str(secs))
+
+            self.write(data)
+            self.flush()
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+
+    application = tornado.web.Application([
+        (r"/", WebshotHandler),
+        (r"/hello", HelloHandler)
+    ], debug=True)
+    application.listen(int(os.environ.get("PORT", 8889)))
+
+    tornado.log.enable_pretty_logging()
+    tornado.ioloop.IOLoop.current().start()
--- rasterize.js
+++ rasterize.js
@@ -1,46 +1,46 @@
 var page = require('webpage').create(),
-    system = require('system'),
-    address, output, size;
+    system = require('system');
 
-if (system.args.length < 3 || system.args.length > 5) {
-    console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
-    console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
-    console.log('  image (png/jpg output) examples: "1920px" entire page, window width 1920px');
-    console.log('                                   "800px*600px" window, clipped to 800x600');
+console.error = function () {
+    require("system").stderr.write(Array.prototype.join.call(arguments, ' ') + '\n');
+};
+
+if (system.args.length < 6 || system.args.length > 7) {
+    console.error('Usage: rasterize.js URL filename width format zoom timeout');
+    console.error('  width: e.g. 1200px');
+    console.error('  format: png, jpg');
+    console.error('  zoom: 1 or 2 (retina)');
+    console.error('  timeout: milliseconds, default = 30000');
     phantom.exit(1);
 } else {
-    address = system.args[1];
-    output = system.args[2];
-    page.viewportSize = { width: 600, height: 600 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
-        size = system.args[3].split('*');
-        if (size.length === 2) {
-            pageWidth = parseInt(size[0], 10);
-            pageHeight = parseInt(size[1], 10);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
-        } else {
-            console.log("size:", system.args[3]);
-            pageWidth = parseInt(system.args[3], 10);
-            pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
-            console.log ("pageHeight:",pageHeight);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-        }
+    var address = system.args[1];
+    var output = system.args[2];
+    var width = parseInt(system.args[3]);
+    var height = width * 3/4;
+    var format = system.args[4];
+    var zoom = parseInt(system.args[5]);
+
+    if (system.args.length == 6) {
+        page.settings.resourceTimeout = system.args[6];
     }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-    }
+
+    page.viewportSize = { width: width, height: height};
+    page.zoomFactor = zoom;
+
+    console.error("address: " + address);
+    console.error("output: " + output);
+    console.error("width: " + width);
+    console.error("height: " + height);
+    console.error("format: " + format);
+    console.error("zoom: " + zoom);
+
     page.open(address, function (status) {
         if (status !== 'success') {
-            console.log('Unable to load the address!');
+            console.error('Unable to load the address!');
             phantom.exit(1);
         } else {
             window.setTimeout(function () {
-                page.render(output);
+                page.render(output, format);
                 phantom.exit();
             }, 200);
         }
